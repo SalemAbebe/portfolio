08700836cdcd77ef685671ad282e5e03be8f0b7a
Salem Abebe <salemabebe@yahoo.com>
2022-11-02 19:46:41 -0400

    import photo

--- src/components/main/About/About.js
+++ src/components/main/About/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../Heading/Header";
+import photo from "../../../assets/images/foto.png";
 
 const About = () => {
   return (
@@ -7,8 +8,20 @@ const About = () => {
       <Header header="About" />
       <div className="about-me">
         {" "}
-        <div className="about-txt"></div>
-        <div className="about-img"></div>
+        <div className="about-txt">
+          {" "}
+          <p>
+            Full stack web developer adapt at building responsive web
+            application from front to back. passionate about learning new
+            technologies, bringing ideas to life, and working with dedicated
+            teams to build efficient and robust application suited to the user
+            needs.
+          </p>
+        </div>
+        <div className="about-img">
+          <img src={photo} alt="my portrait" />
+          <a href="">Resume</a>
+        </div>
       </div>
     </div>
   );
