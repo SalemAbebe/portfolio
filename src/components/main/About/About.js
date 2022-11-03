c74a4285b73801a8ab8960dd26594639d19184b0
Salem Abebe <salemabebe@yahoo.com>
2022-11-02 22:37:46 -0400

    import resume pdf

--- src/components/main/About/About.js
+++ src/components/main/About/About.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./About.scss";
 import Header from "../Heading/Header";
 import photo from "../../../assets/images/foto.png";
-
+import resume from "../../../assets/images/resume-ea.pdf";
 const About = () => {
   return (
     <div className="about-wrapper">
@@ -11,16 +12,17 @@ const About = () => {
         <div className="about-txt">
           {" "}
           <p>
-            Full stack web developer adapt at building responsive web
-            application from front to back. passionate about learning new
-            technologies, bringing ideas to life, and working with dedicated
-            teams to build efficient and robust application suited to the user
-            needs.
+            Full stack web developer adept at building responsive applications
+            from front to backend , passionate about learning new technologies,
+            bringing ideas to life, and working with different teams to build
+            efficient and robust applications suited to user needs.
           </p>
         </div>
         <div className="about-img">
           <img src={photo} alt="my portrait" />
-          <a href="">Resume</a>
+          <a href={resume} target="_blank">
+            Resume
+          </a>
         </div>
       </div>
     </div>
